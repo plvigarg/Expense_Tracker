d49fd22381b238c7570708091bcecf428dafe10a
plvigarg <pallavi312k@gmail.com>
2020-11-13 15:57:10 +0530

    "login logout working"

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from connect import app,db
-from flask import Flask, render_template, session, redirect, url_for,request
+from connect import app, db
+from flask import Flask, render_template, session, redirect, url_for, request
 from flask_login import login_user, logout_user, login_required, current_user
 from forms import loginForm, registrationForm, transactionForm, profiles
-from models import Users,Transactions
+from models import Users, Transactions
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -39,9 +39,16 @@ def index():
             return redirect(next)
     return render_template('index.html', logForm=LoginForm, signForm=RegistrationForm)
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+
+# @app.route('/test')
+# def test():
+#     return render_template('test.html')
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
@@ -51,8 +58,8 @@ def dashboard():
     if transForm.validate_on_submit():
         print("In form")
         data = Transactions(cashFlow=transForm.flow.data,
-                     amount=transForm.amount.data, description=transForm.description.data,cat=transForm.category.data,\
-                         date=transForm.date.data, userId=current_user.id)
+                            amount=transForm.amount.data, description=transForm.description.data, cat=transForm.category.data,
+                            date=transForm.date.data, userId=current_user.id)
 
         db.session.add(data)
         db.session.commit()
@@ -64,11 +71,13 @@ def dashboard():
 
 
 @app.route('/passbook', methods=['GET', 'POST'])
+@login_required
 def passbook():
     return render_template('passbook.html')
 
 
 @app.route('/profile', methods=['GET', 'POST'])
+@login_required
 def profile():
     proForm = profiles()
     return render_template('profile.html', proForm=proForm)
