3a3b7d2c817d9da568b3b30db82d1f24e6b7d2b8
knv.garg <knv.garg@gmail.com>
2021-11-10 18:12:52 +0530

    pip update

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Pallavi\Desktop\Expense_Tracker\env
+set VIRTUAL_ENV=C:\Users\knvga\Desktop\Projects\Expense_Tracker\env
 
 if not defined PROMPT set PROMPT=$P$G
 
